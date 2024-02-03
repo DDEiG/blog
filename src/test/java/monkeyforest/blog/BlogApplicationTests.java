30692ae1b81fd9263299dbd84aedbd4dea0b1ba3
bigth21 <taekhyeon.nam@gmail.com>
2024-02-03 12:25:23 +0900

    Init

---
+++ src/test/java/monkeyforest/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package monkeyforest.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
