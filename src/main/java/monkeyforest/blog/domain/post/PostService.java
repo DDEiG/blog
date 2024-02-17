eeef36be057a62f77edc888262e87526701b211d
bigth21 <taekhyeon.nam@gmail.com>
2024-02-17 13:02:54 +0900

    Polishing

--- src/main/java/monkeyforest/blog/domain/post/PostService.java
+++ src/main/java/monkeyforest/blog/domain/post/PostService.java
@@ -1,7 +1,6 @@
 package monkeyforest.blog.domain.post;
 
 import lombok.RequiredArgsConstructor;
-import lombok.var;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
