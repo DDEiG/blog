fec6812c1d8351091c79045defba24ae22bb7a01
bigth21 <taekhyeon.nam@gmail.com>
2024-02-23 19:54:47 +0900

    Validation 의존성 추가

--- src/main/java/monkeyforest/blog/web/controller/PostController.java
+++ src/main/java/monkeyforest/blog/web/controller/PostController.java
@@ -1,5 +1,7 @@
 package monkeyforest.blog.web.controller;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import monkeyforest.blog.domain.post.entity.Post;
 import monkeyforest.blog.domain.post.service.PostService;
@@ -17,7 +19,8 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping
-    public String findPosts(@RequestParam(defaultValue = "0") int pageNumber, @RequestParam(defaultValue = "10") int pageSize, Model model) {
+    public String findPosts(@RequestParam(defaultValue = "0") @Min(0) int pageNumber,
+                            @RequestParam(defaultValue = "10") @Min(1) int pageSize, Model model) {
         Page<Post> postPage = postService.find(pageNumber, pageSize);
         model.addAttribute("postPage", postPage);
         return "posts";
