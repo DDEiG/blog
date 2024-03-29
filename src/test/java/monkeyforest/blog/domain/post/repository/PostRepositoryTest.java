fa0b41320870cce7b6be81b7803f744a77f09caf
bigth21 <taekhyeon.nam@gmail.com>
2024-03-29 17:21:47 +0900

    Test profile 수정

--- src/test/java/monkeyforest/blog/domain/post/repository/PostRepositoryTest.java
+++ src/test/java/monkeyforest/blog/domain/post/repository/PostRepositoryTest.java
@@ -1,11 +1,14 @@
 package monkeyforest.blog.domain.post.repository;
 
+import monkeyforest.blog.domain.JpaConfig;
 import monkeyforest.blog.domain.post.entity.Post;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -13,7 +16,7 @@ import org.springframework.data.domain.Sort;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JpaConfig.class))
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PostRepositoryTest {
     @Autowired
@@ -25,12 +28,12 @@ class PostRepositoryTest {
         Post post1 = postRepository.save(Post.builder()
                 .title("title1")
                 .body("body1")
-                .username("username1")
+                .writer("username1")
                 .build());
         Post post2 = postRepository.save(Post.builder()
                 .title("title2")
                 .body("body2")
-                .username("username2")
+                .writer("username2")
                 .build());
         // When
         Page<Post> page = postRepository.findAll(PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "id")));
