20d737614ee826bea1f0bf1cc1600b033c840037
bigth21 <taekhyeon.nam@gmail.com>
2024-03-29 16:49:19 +0900

    DataJpaTest 테스트 코드 에러 수정

--- src/test/java/monkeyforest/blog/domain/post/repository/PostRepositoryTest.java
+++ src/test/java/monkeyforest/blog/domain/post/repository/PostRepositoryTest.java
@@ -1,11 +1,14 @@
 package monkeyforest.blog.domain.post.repository;
 
+import monkeyforest.blog.domain.JpaConfig;
 import monkeyforest.blog.domain.post.entity.Post;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -13,7 +16,7 @@ import org.springframework.data.domain.Sort;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JpaConfig.class))
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PostRepositoryTest {
     @Autowired
