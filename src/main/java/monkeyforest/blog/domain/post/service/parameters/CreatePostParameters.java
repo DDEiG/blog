0035f3f68c34d5bb5b0637b40962ad12c46bd944
bigth21 <taekhyeon.nam@gmail.com>
2024-04-11 20:03:50 +0900

    Record의 compact constructor(<-> canonical constructor) 활용 파라미터 validation 추가

--- src/main/java/monkeyforest/blog/domain/post/service/parameters/CreatePostParameters.java
+++ src/main/java/monkeyforest/blog/domain/post/service/parameters/CreatePostParameters.java
@@ -1,8 +1,16 @@
 package monkeyforest.blog.domain.post.service.parameters;
 
 import monkeyforest.blog.domain.post.entity.Post;
+import org.springframework.util.Assert;
 
 public record CreatePostParameters (String title, String body, String writer) {
+
+    public CreatePostParameters {
+        Assert.hasText(title, "Title must not be null");
+        Assert.hasText(body, "Title must not be null");
+        Assert.hasText(writer, "Title must not be null");
+    }
+
     public Post toPost() {
         return new Post(title, body, writer);
     }
