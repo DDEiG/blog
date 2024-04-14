bc326e4cd39d61155db5fcae1b98376d502bda2d
bigth21 <taekhyeon.nam@gmail.com>
2024-04-14 21:23:53 +0900

    500 에러 페이지 추가

--- src/main/java/monkeyforest/blog/web/controller/PostController.java
+++ src/main/java/monkeyforest/blog/web/controller/PostController.java
@@ -28,15 +28,16 @@ public class PostController {
     public String posts(@RequestParam(defaultValue = "") String title,
                         @PageableDefault @SortDefault.SortDefaults({@SortDefault(value = "id", direction = Sort.Direction.DESC)}) Pageable pageable,
                         Model model) {
-        Page<Post> postPage;
-        if (hasText(title)) {
-            postPage = postService.searchPosts(title, pageable);
-        } else {
-            postPage = postService.findPosts(pageable);
-        }
-        model.addAttribute("postPage", postPage);
-        model.addAttribute("title", title);
-        return "post/list";
+        throw new RuntimeException();
+//        Page<Post> postPage;
+//        if (hasText(title)) {
+//            postPage = postService.searchPosts(title, pageable);
+//        } else {
+//            postPage = postService.findPosts(pageable);
+//        }
+//        model.addAttribute("postPage", postPage);
+//        model.addAttribute("title", title);
+//        return "post/list";
     }
 
     @GetMapping("/posts/{id}")
