ebebe766e782c7a5c3adfbf65ad392870b72c489
bigth21 <taekhyeon.nam@gmail.com>
2024-04-15 19:10:19 +0900

    Hardcoded password

---
+++ src/main/java/monkeyforest/blog/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package monkeyforest.blog.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration(proxyBeanMethods = false)
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
