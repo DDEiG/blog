604f5aab60722b7a3c9a5275822918e20b15e04b
bigth21 <taekhyeon.nam@gmail.com>
2024-04-17 22:55:02 +0900

    post 수정 API test code 추가

--- src/test/java/monkeyforest/blog/web/controller/PostControllerTest.java
+++ src/test/java/monkeyforest/blog/web/controller/PostControllerTest.java
@@ -5,6 +5,7 @@ import monkeyforest.blog.domain.post.persistence.repository.PostRepository;
 import monkeyforest.blog.domain.post.service.PostService;
 import monkeyforest.blog.web.controller.form.EditMode;
 import monkeyforest.blog.web.controller.form.PostCreateForm;
+import monkeyforest.blog.web.controller.form.PostModifyForm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,8 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -61,9 +61,9 @@ class PostControllerTest {
     void testPostWrite() throws Exception {
         mockMvc.perform(get("/post/write"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("post/edit"))
                 .andExpect(model().attribute("post", new PostCreateForm()))
-                .andExpect(model().attribute("editMode", EditMode.CREATE));
+                .andExpect(model().attribute("editMode", EditMode.CREATE))
+                .andExpect(view().name("post/edit"));
     }
 
     @Test
@@ -82,6 +82,46 @@ class PostControllerTest {
         var form = new PostCreateForm();
         form.setTitle("haha");
         mockMvc.perform(post("/post")
+                        .flashAttr("post", form))
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/edit"));
+    }
+
+    @Test
+    void testPostModify() throws Exception {
+        given(postService.findPost(any()))
+                .willReturn(Post.builder().build());
+        mockMvc.perform(get("/posts/{id}/modify", 1L))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("post", new PostModifyForm()))
+                .andExpect(model().attribute("editMode", EditMode.UPDATE))
+                .andExpect(view().name("post/edit"));
+    }
+
+    @Test
+    void testModifyPost() throws Exception {
+        var form = new PostModifyForm();
+        form.setId(1L);
+        form.setTitle("haha");
+        form.setBody("hoho");
+        form.setVersion(1L);
+        given(postService.updatePost(any()))
+                .willReturn(Post.builder().id(1L).build());
+        mockMvc.perform(put("/posts/{id}", 1L)
+                .flashAttr("post", form))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/posts/" + 1));
+    }
+
+    @Test
+    void testModifyPostWithoutBody() throws Exception {
+        var form = new PostModifyForm();
+        form.setId(1L);
+        form.setTitle("haha");
+        form.setVersion(1L);
+        given(postService.updatePost(any()))
+                .willReturn(Post.builder().id(1L).build());
+        mockMvc.perform(put("/posts/{id}", 1L)
                         .flashAttr("post", form))
                 .andExpect(status().isOk())
                 .andExpect(view().name("post/edit"));
