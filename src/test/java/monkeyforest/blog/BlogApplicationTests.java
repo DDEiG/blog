92cbd5d338b93726e6c1ca0895667f30ebd76510
bigth21 <taekhyeon.nam@gmail.com>
2024-04-26 00:50:01 +0900

    Polishing

--- src/test/java/monkeyforest/blog/BlogApplicationTests.java
+++ src/test/java/monkeyforest/blog/BlogApplicationTests.java
@@ -3,7 +3,7 @@ package monkeyforest.blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"version = 1.0.0", "group = monkeyforest"})
 class BlogApplicationTests {
 
     @Test
