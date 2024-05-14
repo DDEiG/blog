3820def10a122aebea3a679282d0b39fdc5bfd25
Junghun <hun950803@gmail.com>
2024-05-14 18:53:41 +0900

    on-going: thymeleaf-extra-security 의존성 추가 및 로그인 정보에 따라 화면 다르게 표시 기능 구현

--- src/main/java/monkeyforest/blog/config/AuthenticationProvider.java
+++
@@ -1,4 +0,0 @@
-package monkeyforest.blog.config;
-
-public class AuthenticationProvider {
-}
--- src/main/java/monkeyforest/blog/config/WebSecurityConfig.java
+++ src/main/java/monkeyforest/blog/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package monkeyforest.blog.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
